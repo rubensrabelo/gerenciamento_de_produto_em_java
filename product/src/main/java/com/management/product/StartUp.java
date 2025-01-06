02c1c82bf5ffc5c1e79022b3f33aec98233ddfe4
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-06 16:37:29 -0300

    Renaming main application in spring

--- product/src/main/java/com/management/product/StartUp.java
+++ product/src/main/java/com/management/product/StartUp.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProductApplication {
+public class StartUp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProductApplication.class, args);
+		SpringApplication.run(StartUp.class, args);
 	}
 
 }
