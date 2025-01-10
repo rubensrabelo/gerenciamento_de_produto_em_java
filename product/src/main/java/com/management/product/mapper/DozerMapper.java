407b34458a392a516f50333006d35784d9191bad
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-10 14:20:34 -0300

    Adding mapper with dozer

---
+++ product/src/main/java/com/management/product/mapper/DozerMapper.java
@@ -0,0 +1,23 @@
+package com.management.product.mapper;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.github.dozermapper.core.DozerBeanMapperBuilder;
+import com.github.dozermapper.core.Mapper;
+
+public class DozerMapper {
+	
+	private static Mapper mapper = DozerBeanMapperBuilder.buildDefault();
+	
+	public static <O, D> D parseObject(O origin, Class<D> destination) {
+		return mapper.map(origin, destination);
+	}
+	
+	public static <O, D> List<D> parseListObjects(List<O> origin, Class<D> destination) {
+		List<D> destinationObjects = origin.stream()
+				.map(o -> mapper.map(o, destination))
+				.collect(Collectors.toList());
+		return destinationObjects;
+	}
+}
