e4647e8c866733c0741729a9557a0984f9ebc9c5
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-01-27 13:45:35 -0300

    Fixing GeneratedValue in the product model

--- product/src/main/java/com/management/product/models/Product.java
+++ product/src/main/java/com/management/product/models/Product.java
@@ -17,7 +17,7 @@ public class Product implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
 	@Column(unique = true, nullable = false)
