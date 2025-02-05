1f61080c3244181125ca45ec1256b845d7b64d2b
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-02-05 15:55:01 -0300

    Adding RequiredObjectIsNullException.java in the services exceptions

--- product/src/main/java/com/management/product/services/exceptions/RequiredObjectIsNullException.java
+++ product/src/main/java/com/management/product/services/exceptions/RequiredObjectIsNullException.java
@@ -1,4 +1,12 @@
 package com.management.product.services.exceptions;
 
-public class RequiredObjectIsNullException {
+public class RequiredObjectIsNullException extends RuntimeException {
+
+    public RequiredObjectIsNullException() {
+        super("It is not allowed to persist a null object!");
+    }
+
+    public RequiredObjectIsNullException(String msg) {
+        super(msg);
+    }
 }
