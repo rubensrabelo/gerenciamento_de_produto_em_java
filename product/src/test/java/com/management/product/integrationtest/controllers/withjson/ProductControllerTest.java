a91262a289374b0c81b5a8597737b940786f8087
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-06 14:31:37 -0300

    Starting test in the controller

---
+++ product/src/test/java/com/management/product/integrationtest/controllers/withjson/ProductControllerTest.java
@@ -0,0 +1,97 @@
+package com.management.product.integrationtest.controllers.withjson;
+
+import com.management.product.config.TestConfigs;
+import com.management.product.integrationtest.dto.ProductDTO;
+import com.management.product.integrationtest.testcontainers.AbstractIntegrationTest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.DeserializationFeature;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.MediaType;
+
+import java.io.IOException;
+
+import static io.restassured.RestAssured.given;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@TestMethodOrder((MethodOrderer.OrderAnnotation.class))
+class ProductControllerTest extends AbstractIntegrationTest {
+
+    private static RequestSpecification specification;
+    private static ObjectMapper objectMapper;
+
+    private static ProductDTO product;
+
+    @BeforeAll
+    static void setUp() {
+        objectMapper = new ObjectMapper();
+        objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+
+        product = new ProductDTO();
+    }
+
+    @Test
+    @Order(1)
+    void create() throws IOException {
+        mockProduct();
+
+        specification = new RequestSpecBuilder()
+                .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_LOCAL)
+                .setBasePath("/products")
+                .setPort(TestConfigs.SERVER_PORT)
+                    .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+                    .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+            .build();
+
+        var content = given(specification)
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                    .body(product)
+                .when()
+                    .post()
+                .then()
+                    .statusCode(201)
+                .extract()
+                    .body()
+                    .toString();
+
+        ProductDTO createdProduct = objectMapper.readValue(content, ProductDTO.class);
+
+        assertNotNull(createdProduct.getId());
+        assertNotNull(createdProduct.getName());
+        assertNotNull(createdProduct.getPrice());
+
+        assertTrue(createdProduct.getId() > 0);
+
+        assertEquals("Product 01", createdProduct.getName());
+        assertEquals(1.00, createdProduct.getPrice());
+
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    private void mockProduct() {
+        product.setName("Product 01");
+        product.setPrice(1.00);
+    }
+}
---
+++ product/src/test/java/com/management/product/integrationtest/dto/ProductDTO.java
@@ -0,0 +1,57 @@
+package com.management.product.integrationtest.dto;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class ProductDTO implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String name;
+	private Double price;
+	
+	public ProductDTO() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id, name, price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ProductDTO other = (ProductDTO) obj;
+		return Objects.equals(id, other.id) && Objects.equals(name, other.name) && Objects.equals(price, other.price);
+	}
+}
