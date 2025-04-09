84faeb26bcc2ebb7f84648526979043a9c2d3596
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-09 09:49:37 -0300

    Adding productIsNotInStock method in the ProductRepository

--- product/src/main/java/com/management/product/repositories/ProductRepository.java
+++ product/src/main/java/com/management/product/repositories/ProductRepository.java
@@ -3,6 +3,20 @@ package com.management.product.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.management.product.models.Product;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Modifying(clearAutomatically = true)
+    @Transactional
+    @Query("""
+        UPDATE Product p
+        SET p.isInStock = false
+        WHERE p.id = :id
+
+    """)
+    void productIsNotInStock(@Param("id") Long id);
 }
